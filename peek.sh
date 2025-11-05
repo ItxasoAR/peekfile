260f6602c73dff23d68d5e907d57c4bb34a0b9a3
ItxasoAR <itxasoalonsorodriguez@gmail.com>
2025-11-05 20:15:45 +0100

    third commit

--- peek.sh
+++ peek.sh
@@ -1,3 +1,12 @@
-(head -n $2 $1;
+
+#if [[ -z $2 ]] ;
+#then $2=3; fi
+
+[[ -z $2 ]] && set -- $1 3
+
+if [[ $(wc -l $1) -le $((2*$2)) ]] ; 
+then cat $1; 
+else head -n $2 $1;
 echo ...; 
-tail -n $2 $1)
+tail -n $2 $1; fi
+
